3f4208f68f15e68f7ac27957e628d596eaaef439
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-25 15:13:37 +0200

    Working on generate-results.

--- actions/generate_results.py
+++ actions/generate_results.py
@@ -7,14 +7,20 @@ from actions import utils
 def generate_results(args):
     utils.assert_is_executable(args.script)
 
-    cmd = [
+    target_dir = args.target.format(solver = os.path.basename(args.solver))
+    cmd = list(map(str, [
         args.script,
         '-p', args.partition,
         '--time-limit', args.timeout,
         '--solver-options', args.solver_options,
         '--benchmark-sets', args.benchmarks,
-        '--working-dir', args.target.format(solver = os.path.basename(args.solver)),
+        '--working-dir', target_dir,
         args.solver
-    ]
+    ]))
     logging.info('Executing {}'.format(' '.join(map(str, cmd))))
-    # subprocess.call(cmd)
+    if not args.dry:
+        subprocess.call(cmd)
+
+    print('When all jobs for the solvers has finished, collect the results as follows:')
+    print('\t{} collect-results {} ...'.format(sys.argv[0], target_dir))
+
