f7014d662aeca46fed57d35d5148812e58064fbd
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-25 17:48:40 +0200

    Fixed filenames.

--- actions/train.py
+++ actions/train.py
@@ -11,13 +11,13 @@ def build_models(args):
         subprocess.call(['make'], cwd = 'models')
 
 def train_model(args, binary, modelfile):
-    logging.info('training {}'.format(binary))
+    logging.info('training {} to {}'.format(binary, modelfile))
     if not args.dry:
         subprocess.call([binary, args.data, modelfile], cwd = 'models')
 
 def train(args):
     build_models(args)
     for model in glob.glob('models/train_*.cpp'):
-        binary = model[:4]
-        modelfile = binary[6:] + '.model'
+        binary = model[:-4]
+        modelfile = binary[7:] + '.model'
         train_model(args, binary, modelfile)
